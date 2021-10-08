27f2de433e0fffba0118a72f65a4f72a434501f8
Julia Song <julja.song@gmail.com>
2021-10-08 17:32:31 +0900

    20211008_Http Request - API Message Body - JOSN

---
+++ src/main/java/hello/servlet/basic/HelloData.java
@@ -0,0 +1,12 @@
+package hello.servlet.basic;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter @Setter // Lombok
+public class HelloData {
+
+    private String username;
+    private int age;
+
+}
